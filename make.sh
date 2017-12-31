63d9df0b8f9b36d36031779e50f1bf266ba6ae92
cunidev <raffaele.tranquillini@gmail.com>
2017-12-31 15:28:52 +0100

    working make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,9 @@
-zip -r Gestures.zip src/*
+cd src
+zip -r ../Gestures.zip *
+cd ..
 echo '#!/usr/bin/env python' | cat - Gestures.zip > Gestures_0.1
+rm Gestures.zip
 chmod +x Gestures_0.1
-
+mkdir build
+mv Gestures_0.1 build/
+cp gestures.desktop build/
