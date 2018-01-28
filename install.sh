f8b84dcc552ecedd9d46de4ca2aa432506d08015
Unknown <raffaele.tranquillini@gmail.com>
2018-01-28 17:45:46 +0100

    add temporary svg app icon

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 sudo mkdir /opt/gestures
 sudo cp ./build/gestures /opt/gestures/
 sudo cp ./build/gestures.desktop /usr/share/applications/
-
+sudo cp ./gestures-temporary.svg /opt/gestures/gestures.svg
