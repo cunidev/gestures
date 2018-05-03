95758bd381bc26e4c66801869e0f328663ae6899
cunidev <raffaele.tranquillini@gmail.com>
2018-05-03 18:49:08 +0200

    installer fixes

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 mkdir /opt/gestures
 cp ./build/gestures /opt/gestures/
+sed -e 's/\/usr\/bin/\/opt\/gestures/g' -i ./build/org.cunidev.gestures.desktop 
 cp ./build/org.cunidev.gestures.desktop /usr/share/applications/
 cp ./data/org.cunidev.gestures.svg /usr/share/icons/hicolor/scalable/apps
